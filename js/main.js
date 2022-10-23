f07e796282852c8d4fb8c5cd6b2d37a1dbc93541
Cedrus81 <erez.eitan.97@gmail.com>
2022-10-23 15:14:05 +0300

    finished collapsing menu modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-function onOpenMenu() {
 
+function onToggleMenu() {
+    document.body.classList.toggle('menu-open')
+}
+
+function onSelectCategory(elBtn) {
+    const elNavBtns = document.querySelectorAll('.btn-nav')
+    for (const btn of elNavBtns) {
+        if (btn.innerText === elBtn.innerText) btn.classList.add('selected')
+        else btn.classList.remove('selected')
+    }
 }
