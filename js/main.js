e77d701650fff1e9cac3a722bb5c30666d668380
Cedrus81 <erez.eitan.97@gmail.com>
2022-10-23 21:17:40 +0300

    finished last features

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 
-function onToggleMenu() {
-    document.body.classList.toggle('menu-open')
-}
+
 
 function onSelectCategory(elBtn) {
-    const elNavBtns = document.querySelectorAll('.btn-nav')
+    const elNavBtns = elBtn.parentElement.querySelectorAll('.btn-nav')
+    if (elBtn.parentElement.classList.contains('dropdown')) {
+        onSelectCategory(elBtn.parentElement)
+    }
     for (const btn of elNavBtns) {
         if (btn.innerText === elBtn.innerText) btn.classList.add('selected')
         else btn.classList.remove('selected')
     }
+}
+
+function toggleDropDown(elDrop) {
+    let elMenu = elDrop.querySelector('.dropdown-menu')
+    elMenu.classList.add('active')
+}
+function hideDropDown(elDrop) {
+    let elMenu = elDrop.querySelector('.dropdown-menu')
+    elMenu.classList.remove('active')
+}
+
+function onReadMore() {
+    toggleScreen('read')
+}
+
+function onToggleMenu() {
+    toggleScreen('menu')
+}
+
+
+function toggleScreen(modal) {
+    let elBody = document.querySelector('body')
+    elBody.classList.toggle('screen-modal')
+    if (modal) elBody.classList.toggle(modal)
+    else elBody.classList.remove('menu', 'read')
 }
